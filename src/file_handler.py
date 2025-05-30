4244d98355609da47c09d8673d098c778eb96151
bgosha24 <bgosha24@gmail.com>
2025-05-30 09:57:57 -0600

    added FileHandler class

---
+++ src/file_handler.py
@@ -0,0 +1,52 @@
+# Key Responsibilities:
+    # Opening and closing the database file.
+    # Reading a specified number of bytes from a given offset.
+    # Writing a specified number of bytes to a given offset.
+    # Appending bytes to the end of the file.
+    # Reporting the current size of the file.
+
+import os
+
+class FileHandler:
+    
+    def __init__(self,file_path):
+        self.file_path = file_path
+
+        # create the file in the init call so that you can refer to it later. Everytime you instantiate FileHandler you will be working with an open file anyway
+        if not os.path.exists(self.file_path):
+            self.file = open(file_path,'w+b')
+        else:
+            self.file = open(file_path, 'r+b')
+      
+    def close(self):
+        # FileHanlder.close()
+        self.file.close()
+
+
+
+    # given a specific offset, write bytes to that offset in the file
+    def write_bytes(self,offset:int,bytes_to_write:bytes):
+        self.file.seek(offset)
+        self.file.write(bytes_to_write)
+        self.file.flush()
+
+    # given a specific offset, read from that offset in the file
+    def read_bytes(self,offset:int,number_of_bytes_to_read:int):
+        self.file.seek(offset)
+        read_bytes = self.file.read(number_of_bytes_to_read)
+        return read_bytes
+
+
+    def append_bytes(self,bytes_to_append:bytes):
+        self.file.seek(0,os.SEEK_END)
+        self.file.write(bytes_to_append)
+        self.file.flush()
+    
+    @property
+    def file_size(self):
+        self.file.seek(0,os.SEEK_END)
+        return self.file.tell()
+
+
+
+
---
+++ tests/test_file_handler.py
@@ -0,0 +1,37 @@
+import os
+from src.file_handler import FileHandler
+
+
+def test_file_handler():
+    test_path = "example.db"
+
+    if os.path.exists(test_path):
+        os.remove(test_path)
+
+    handler = FileHandler(test_path)
+
+    with open("example.db","wb") as f:
+        f.write(b'420')
+        f.close()
+
+    with open("example.db",'rb') as f:
+        read_data = f.read()
+        f.close()
+    # read_bytes should work same as f.read()
+    assert read_data == handler.read_bytes(0,9999) , "Failed to read bytes"
+
+    # write at specific offset
+    handler.write_bytes(0, b'420')
+    assert handler.read_bytes(0, 3) == b'420', "Failed to read written bytes"
+    assert handler.file_size == 3 , "file size not added correctly"
+
+    # append to a file
+    handler.append_bytes(b'69')
+
+    assert handler.read_bytes(0,9999) == b'42069', "Failed to append bytes"
+
+    handler.close()
+
+    # reset test
+    os.remove(test_path)
+    print(f"FileHandler class tests passed")
